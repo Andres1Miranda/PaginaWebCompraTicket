96d6f824e232231519eb8e063d63582fece90580
Andres1Miranda <atai.angel4@gmail.com>
2025-07-21 23:40:35 -0500

    actualizacion

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
-const clientesRouters = require("./src/Routers/clienteRouters")
-const express = require("express")
-const app =express()
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+const app =express()
 const port = 3000
 
 app.use(express.json())
 
-app.set("view engine","ejs");
+app.use(express.static(__dirname + "/"));
 
 app.get("/",function(req,res){
-    res.render("index")
-})
+    res.sendFile(__dirname + "/views/index.html")
+});
+app.get("/informacion",function(req,res){
+    res.sendFile(__dirname + "/informacion.html")
+});
+app.get("/compra",function(req,res){
+    res.sendFile(__dirname + "/compra.html")
+});
+
 app.listen(port,function(){
     console.log('Servidor corriendo en el puerto 3000')
 })
